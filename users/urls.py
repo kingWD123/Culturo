989a82855d9bdfa9a192f7a615d0b014de57d4f2
soro <sorolj@ept.sn>
2025-07-15 17:27:11 +0000

    pour charbel

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('cinema-recommandations/', views.CinemaRecommandations, name='cinema_recommandations'),
     path('cinema_chatbot_api/', views.cinema_chatbot_api, name='cinema_chatbot_api'),
+    path('cinema_recommendations_result/', views.cinema_recommendations_result, name='cinema_recommendations_result'),
 ] 
