71a41a035d423e6c0a698e293a5dcd4d97bb2b75
acharbel54 <acharbeladonis@ept.sn>
2025-07-30 10:21:46 +0000

    requirements add and procfile

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('profile/', views.profile_view, name='profile'),
     
     # Cinema
     path('cinema-recommandations/', views.CinemaRecommandations, name='cinema_recommandations'),
